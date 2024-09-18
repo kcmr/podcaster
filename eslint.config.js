a7938ddec99bc50c67c8df596be7a85a420940b0
Kus Cámara <kcmr@users.noreply.github.com>
2024-09-18 21:15:53 +0200

    add prettier config to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,22 +3,41 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+import reactPlugin from 'eslint-plugin-react'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierConfig,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
+      react: reactPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'react/jsx-boolean-value': ['error', 'never'],
+      'react/jsx-no-useless-fragment': 'error',
+      'react/self-closing-comp': [
+        'error',
+        {
+          component: true,
+          html: false,
+        },
+      ],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
