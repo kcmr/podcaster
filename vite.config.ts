0d13c8a6e971694a5979852889f8d53850a0e23b
Kus Cámara <kcmr@users.noreply.github.com>
2024-09-18 23:16:22 +0200

    setup tailwind and postcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
