a364982eb9709049bd3f82d4d95d839ed131ea06
Kus Cámara <kcmr@users.noreply.github.com>
2024-09-21 18:32:49 +0200

    add search functionality to home page

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,13 +1,34 @@
 import { useLoaderData } from 'react-router-dom'
-import { PodcastList } from '@/components'
+import { PodcastList, Search } from '@/components'
 import type { Podcast } from '@/types'
+import { useSearchStore } from '@/store'
 
 export default function Home() {
   const podcasts = useLoaderData() as Podcast[]
+  const { searchTerm, setSearchTerm } = useSearchStore()
+
+  const handleSearchChange = (value: string) => {
+    setSearchTerm(value)
+  }
+
+  const filteredPodcasts = podcasts.filter(
+    (podcast) =>
+      podcast.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      podcast.author.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      podcast.description.toLowerCase().includes(searchTerm.toLowerCase()),
+  )
 
   return (
-    <div className="py-16">
-      <PodcastList podcasts={podcasts} />
+    <div>
+      <Search
+        value={searchTerm}
+        placeholder="Filter podcasts…"
+        onChange={handleSearchChange}
+        className="mt-2 mb-4"
+      />
+      <div className="py-16">
+        <PodcastList podcasts={filteredPodcasts} />
+      </div>
     </div>
   )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -71,3 +71,13 @@ export const usePodcastDetailStore = create(
     },
   ),
 )
+
+interface SearchStore {
+  searchTerm: string
+  setSearchTerm: (term: string) => void
+}
+
+export const useSearchStore = create<SearchStore>((set) => ({
+  searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
+}))
