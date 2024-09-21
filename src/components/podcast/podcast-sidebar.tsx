601472ea8bb02055fbb2b4a408e9b71a5613a714
Kus Cámara <kcmr@users.noreply.github.com>
2024-09-21 10:40:06 +0200

    link title and image in podcast sidebar to podcast detail page

--- src/components/podcast/podcast-sidebar.tsx
+++ src/components/podcast/podcast-sidebar.tsx
@@ -2,27 +2,34 @@ import type { ComponentProps } from 'react'
 import type { Podcast } from '../../types'
 import Card from '../common/card'
 import { cn } from '../../utils/style'
+import { Link } from 'react-router-dom'
 
 interface PodcastSidebarProps extends ComponentProps<'div'> {
   podcast: Podcast
 }
 
 export function PodcastSidebar({ podcast, className }: PodcastSidebarProps) {
-  const { title, author, imageSrc, description } = podcast
+  const { title, author, imageSrc, description, id } = podcast
 
   return (
     <Card className={cn('flex flex-col gap-4', className)}>
-      <img
-        src={imageSrc}
-        alt={`${title} cover`}
-        width={148}
-        height={148}
-        className="rounded-md mx-auto"
-      />
-      <div className="border-t pt-3.5">
-        <h2 className="font-bold">{title}</h2>
-        <p className="text-sm text-gray-600">by {author}</p>
-      </div>
+      <Link
+        to={`/podcast/${id}`}
+        aria-label={`Go to ${title} podcast page`}
+        className="flex flex-col gap-4"
+      >
+        <img
+          src={imageSrc}
+          alt={`${title} cover`}
+          width={148}
+          height={148}
+          className="rounded-md mx-auto"
+        />
+        <div className="border-t pt-3.5">
+          <h2 className="font-bold">{title}</h2>
+          <p className="text-sm text-gray-600">by {author}</p>
+        </div>
+      </Link>
       <div className="border-t pt-3.5">
         <h3 className="font-bold text-sm mb-2">Description:</h3>
         <p className="text-sm text-gray-600 italic">{description}</p>
