882992582e96c4b32785707982824e15dd71da86
Kus Cámara <kcmr@users.noreply.github.com>
2024-09-22 13:37:54 +0200

    improve responsive styles for podcast detail

--- src/pages/podcast/root.tsx
+++ src/pages/podcast/root.tsx
@@ -6,9 +6,9 @@ export default function Podcast() {
   const podcastDetail = useLoaderData() as PodcastDetail
 
   return (
-    <div className="flex gap-8">
+    <div className="flex flex-wrap gap-x-8 gap-y-4">
       <PodcastSidebar
-        className="w-[230px] flex-shrink-0 self-start"
+        className="w-full md:w-[230px] flex-shrink-0 self-start"
         podcast={podcastDetail}
       />
       <div className="flex-1 flex flex-col gap-4">
